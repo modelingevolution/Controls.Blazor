1681231bd465580f89766ce680729b6c35f855cf
Rafał Maciąg <rafal.maciag@modelingevolution.com>
2024-04-26 17:12:46 +0200

    StreamServer

---
+++ Source/ModelingEvolution.Controls.Blazor.AnchorPanelControl/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+
+namespace ModelingEvolution.Controls.Blazor
+{
+    public static class Extensions
+    {
+        public static async Task ToggleFullScreen(this IJSRuntime runtime, string controlId)
+        {
+            await runtime.InvokeVoidAsync("toggleFullScreen", controlId);
+        }
+    }
+}
---
+++ Source/ModelingEvolution.Controls.Blazor.AnchorPanelControl/wwwroot/anchor.js
@@ -0,0 +1,9 @@
+﻿
+window.toggleFullScreen = function (id) {
+    const elem = document.getElementById(id);
+    if (!document.fullscreenElement) {
+        elem.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+};
