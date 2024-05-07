0ee9ea667d6706edbc3bf460d9f3a6edc0094329
Rafał Maciąg <rafal.maciag@modelingevolution.com>
2024-05-07 21:38:42 +0200

    Histogram

--- Source/ModelingEvolution.Controls.Blazor.HistogramControl/Class1.cs
+++ Source/ModelingEvolution.Controls.Blazor.HistogramControl/Class1.cs
@@ -15,7 +15,12 @@ namespace ModelingEvolution.Controls.Blazor.HistogramControl
     {
         readonly List<PointD> _points = new();
         readonly List<PointIndex> _indexes = new();
-
+        public event Func<byte[], Task> Completed; 
+        public double Width
+        {
+            get => _points[^1].X;
+            set => _points[^1] = new PointD(value, _points[^1].Y);
+        }
         public HistogramSeriesVm()
         {
             _points.Add(new Point<double>(0, 0));
@@ -23,7 +28,7 @@ namespace ModelingEvolution.Controls.Blazor.HistogramControl
             _indexes.Add(new PointIndex(0, _points));
             _indexes.Add(new PointIndex(1, _points));
         }
-
+        public IEnumerable<BezierCurveD> Curves => BezierCurveD.Create(Points, 0.3d);
         public IReadOnlyList<PointD> Points => _points;
         public IReadOnlyList<PointIndex> Indexes => _indexes;
 
@@ -37,12 +42,39 @@ namespace ModelingEvolution.Controls.Blazor.HistogramControl
                 _indexes[i].Index = i;
             }
         }
+
+        public async Task Complete()
+        {
+            if (Indexes.Any(x => x.IsDirty))
+            {
+                byte[] result = new byte[255];
+
+                var d = Width;
+                foreach (var c in Curves)
+                {
+                    for (double t = 0; t <= 1; t += 0.002d)
+                    {
+                        var p = c.Evaluate(t);
+                        var x = (int)(p.X / d * 255d);
+                        result[x] = (byte)p.Y;
+                    }
+                }
+
+                foreach(var i in Indexes)
+                    i.Clear();
+
+                await Completed(result);
+            }
+        }
     }
 
     class HistogramVm
     {
         private readonly List<HistogramSeriesVm> _series = new();
         private HistogramSeriesVm _selected;
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public HistogramSeriesVm Selected => _selected;
         public void Register(HistogramSeriesVm series)
         {
             _series.Add(_selected = series);
@@ -52,14 +84,48 @@ namespace ModelingEvolution.Controls.Blazor.HistogramControl
         {
             _selected.Add(point);
         }
+
+        public Task OnMouseUp() => _selected.Complete();
     }
+    
     class PointIndex(int ix, List<PointD> items)
     {
         public int Index { get; set; } = ix;
         public PointD Location
         {
             get => items[Index];
-            set => items[Index] = value;
+            set
+            {
+                items[Index] = value;
+                IsDirty = true;
+            }
+        }
+
+        public bool IsDirty { get; private set; }
+        public void Clear() => IsDirty = false;
+        public bool CanMove(PointD arg, out PointD result)
+        {
+            result = arg;
+            if (Index > 0)
+            {
+                if (arg.X <= items[Index - 1].X)
+                    return false;
+            }
+            else 
+                result = new PointD(0, result.Y);
+
+            if (Index < items.Count - 1)
+            {
+                if (arg.X >= items[Index + 1].X)
+                    return false;
+            }
+            else
+            {
+                var last = items.Last();
+                result = new PointD(last.X, result.Y);
+            }
+
+            return true;
         }
     }
 }
--- Source/ModelingEvolution.Controls.Blazor.SvgCanvasControl/MouseService.cs
+++ Source/ModelingEvolution.Controls.Blazor.SvgCanvasControl/MouseService.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.JSInterop;
 using ModelingEvolution.Drawing;
@@ -23,12 +24,50 @@ namespace ModelingEvolution.Controls.Blazor.SvgCanvasControl
         Bottom,
         Left
     }
-
+    public delegate bool MoveDelegate(Point<double> actual, out Point<double> expected);
     public static class MouseExtensions
     {
         public static Vector<double> ClientMouseCoordinates(this MouseEventArgs e)
         {
             return new Vector<double>(e.ClientX, e.ClientY);
+        }
+    }
+
+    public class CanvasMouseEventArgs
+    {
+        public Point<double> Location { get; init; }
+        public MouseEventArgs Args { get; init; }
+    }
+    public record ViewBox
+    {
+        public static ViewBox Create(double x, double y, double w, double h)
+        {
+            return new ViewBox() { Offset = new Vector<double>(x, y), Size = new Size<double>(w, h) };
+        }
+        public Size<double> Size { get; set; }
+        public Vector<double> Offset { get; set; }
+
+        public Size<double> ElementSize { get; internal set; }
+        public override string ToString()
+        {
+            return string.Format(CultureInfo.InvariantCulture, "{0} {1} {2} {3}", Offset.X, Offset.Y, Size.Width, Size.Height);
+        }
+
+        public Point<double> Calculate(double x, double y)
+        {
+            var dstX = x * Size.Width / ElementSize.Width;
+            var dstY = y * Size.Height / ElementSize.Height;
+            return new Point<double>(dstX, dstY);
+        }
+        public Point<double> Calculate(Point<double> point)
+        {
+            return point * Size / ElementSize;
+            
+        }
+
+        public Vector<double> Calculate(Vector<double> point)
+        {
+            return point * Size / ElementSize;
         }
     }
 }
